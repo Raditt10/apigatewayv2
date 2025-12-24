035f806008b11e06fb9d310c569b5e1abbac40a8
Raditt10 <iniakuraditt@gmail.com>
2025-12-24 18:48:06 +0700

    Trigger CI/CD

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 app = Flask(__name__)
 
-# Konfigurasi AWS 1
+# Konfigurasi AWS 
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_SESSION_TOKEN = os.getenv("AWS_SESSION_TOKEN")
@@ -16,7 +16,7 @@ AWS_REGION = os.getenv("AWS_REGION")
 S3_BUCKET = os.getenv("S3_BUCKET_NAME")
 API_URL = os.getenv("API_GATEWAY_URL")
 
-# trigger lagi 4
+# trigger lagi ke 5
 s3_client = boto3.client(
     "s3",
     aws_access_key_id=AWS_ACCESS_KEY_ID,
